459cfb68b44a6bb830b8789250968d8433da2afe
Nazib Md. Badrul <61074679+Najib199@users.noreply.github.com>
2023-11-11 18:01:32 +0600

    Add files via upload

---
+++ Assignment-7/app/Http/Controllers/UserController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller {
+    public function index() {
+        return '<center><h1>Hello, Laravel!</h1></center>';
+    }
+}
